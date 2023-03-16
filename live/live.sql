c7d41f24f3cb7723215c232a7ab870667a68a247
leevle <49538642@qq.com>
2023-03-16 17:57:05 +0800

    结构更新

--- live/live.sql
+++ live/live.sql
@@ -20,18 +20,25 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 -- Table structure for live
 -- ----------------------------
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for live
+-- ----------------------------
 DROP TABLE IF EXISTS `live`;
 CREATE TABLE `live`  (
                          `id` int(11) NOT NULL AUTO_INCREMENT,
                          `uuid` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
                          `push_code` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+                         `push_token` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
                          `ban` tinyint(4) UNSIGNED ZEROFILL NOT NULL DEFAULT 0000,
                          `client_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
                          `stream_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
-                         `push_token` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
                          PRIMARY KEY (`id`) USING BTREE,
                          UNIQUE INDEX `uuid`(`uuid`) USING BTREE,
                          UNIQUE INDEX `push_code`(`push_code`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
